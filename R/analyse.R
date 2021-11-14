dbcac0e0c8fef0caccb4d53a5d763b4cf3a7f4dd
hsonne <hauke.sonnenberg@gmx.de>
2021-11-14 23:33:31 +0100

    Add minimal documentation on analyse()

--- R/analyse.R
+++ R/analyse.R
@@ -1,4 +1,20 @@
 # analyse ----------------------------------------------------------------------
+
+#' Analyse the Parse Tree of an R Script
+#'
+#' @param x parse tree as returned by \code{\link{parse}}
+#' @param path for internal use only (when this function is called recursively)
+#' @return list representing type information on the nodes in the parse tree
+#' @export
+#' @examples
+#' # Parse an R script file (here, a file from kwb.utils)
+#' x <- parse("https://raw.githubusercontent.com/KWB-R/kwb.utils/master/R/column.R")
+#' 
+#' # Analyse the parse tree (This may take some time!)
+#' result <- kwb.code::analyse(x)
+#' 
+#' # Show the structure of the result list (only 3 levels!)
+#' str(result, 3)
 analyse <- function(x, path = "")
 {
   info <- type_info(x)
