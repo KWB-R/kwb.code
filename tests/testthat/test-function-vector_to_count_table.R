de7da4c307957a932709b1614746d7336a1033d4
hsonne <hauke.sonnenberg@gmx.de>
2021-11-27 18:45:49 +0100

    Implement some tests

--- tests/testthat/test-function-vector_to_count_table.R
+++ tests/testthat/test-function-vector_to_count_table.R
@@ -6,5 +6,13 @@ test_that("vector_to_count_table() works", {
 
   expect_null(f(character()))
 
-  f(c("x", "x", "y", "x", "y"))
+  result <- f(c("x", "x", "y", "x", "y"))
+  
+  expect_identical(
+    result, 
+    kwb.utils::noFactorDataFrame(
+      name = c("x", "y"), 
+      count = c(3L, 2L)
+    )
+  )
 })
