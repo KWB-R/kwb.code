4a401e5be1855e198417a90c2d844798acf20200
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-08-12 05:20:31 +0200

    Move functions to where they are used

--- R/analyse.R
+++ R/analyse.R
@@ -36,3 +36,58 @@ analyse <- function(x, path = "")
   
   result  
 }
+
+# type_info --------------------------------------------------------------------
+type_info <- function(x, as.character = FALSE)
+{
+  #shorten <- function(x) kwb.utils::shorten(x, max_chars = 30L)
+  shorten <- function(x) paste(substr(x, 1, 30), "...")
+  
+  text <- as.character(x)
+  
+  info <- list(
+    type = typeof(x),
+    mode = mode(x),
+    class = class(x), 
+    length = length(x),
+    text = shorten(paste0("[", seq_along(text), "]", text, collapse = "")),
+    is = if (length(x) == 1L) {
+      suppressWarnings(is_what(x, silent = TRUE))
+    }
+  )
+  
+  info <- c(info, list(
+    n_modes = length(info$mode),
+    n_classes = length(info$class)
+  ))
+  
+  if (! as.character) {
+    return(info)
+  }
+  
+  info_to_text(info)
+}
+
+# info_to_text -----------------------------------------------------------------
+#' @importFrom kwb.utils commaCollapsed
+info_to_text <- function(info)
+{
+  collapse <- function(element) {
+    kwb.utils::selectElements(info, element) %>% 
+      kwb.utils::commaCollapsed()
+  }
+  
+  #prefix <- "type|mode|class|length|is: "
+  prefix <- NULL
+  
+  paste0(
+    prefix, 
+    paste(collapse = "|", c(
+      collapse("type"),
+      collapse("mode"),
+      collapse("class"), 
+      collapse("length"),
+      collapse("is")
+    )) 
+  )
+}
